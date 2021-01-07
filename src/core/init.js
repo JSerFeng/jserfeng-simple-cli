f51003be6943a4cd37df5c04d2e674f465797888
FY1010 <1114550440@qq.com>
2021-01-07 21:09:23 +0800

    update.

--- src/core/init.js
+++ src/core/init.js
@@ -1,6 +1,6 @@
 const shell = require('shelljs')
-const {writeFile, resolvePath, joinPath} = require('../utils/helper')
-const {tpl} = require('../templates')
+const { writeFile, resolvePath, joinPath } = require('../utils/helper')
+const { tpl } = require('../templates')
 const createPage = require('./createPage')
 
 const init = async (projectName, options) => {
@@ -15,25 +15,24 @@ const init = async (projectName, options) => {
   const router = optionMap.has('router')
 
   const ext = ts ? '.ts' : '.js'
-  const extx = ext + 'x'
   /* 创建react项目 */
-  const {code} = shell.exec(`npx create-react-app ${projectName} ${ts ? '--template typescript' : ''}`)
+  const { code } = shell.exec(`npx create-react-app ${projectName} ${ts ? '--template typescript' : ''}`)
   if (code !== 0) {
     console.log("some error happens when create-react-app running")
     return
   }
   shell.cd(projectName)
   const projectDir = shell.pwd().stdout
-  await installDep(redux, router, axios, ts)
+  installDep({redux, router, axios, ts})
 
-  writeFile(joinPath(projectDir, 'jsf.config.js'), tpl('config', {ts, redux}))
-  initDirections({redux, axios, router})
+  writeFile(joinPath(projectDir, 'jsf.config.js'), tpl('config', { ts, redux }))
+  initDirections({ redux, axios, router })
   if (redux) {
     console.log('create redux file')
-    const tplIndex = tpl('tpl_redux_index', {ts})
-    const tplReducer = tpl('tpl_reducer', {ts})
-    const tplActions = tpl('tpl_actions', {ts})
-    const tplConstants = tpl('tpl_constants', {ts})
+    const tplIndex = tpl('tpl_redux_index', { ts })
+    const tplReducer = tpl('tpl_reducer', { ts })
+    const tplActions = tpl('tpl_actions', { ts })
+    const tplConstants = tpl('tpl_constants', { ts })
     writeFile(joinPath(projectDir, `/src/redux/index${ext}`), tplIndex)
     writeFile(joinPath(projectDir, `/src/redux/reducer${ext}`), tplReducer)
     writeFile(joinPath(projectDir, `/src/redux/actions${ext}`), tplActions)
@@ -41,8 +40,8 @@ const init = async (projectName, options) => {
   }
   if (axios) {
     console.log('create axios file')
-    const tplIndex = tpl('tpl_axios_config', {ts})
-    const tplApi = tpl('tpl_api', {ts})
+    const tplIndex = tpl('tpl_axios_config', { ts })
+    const tplApi = tpl('tpl_api', { ts })
     writeFile(joinPath(projectDir, `/src/services/index${ext}`), tplIndex)
     writeFile(joinPath(projectDir, `/src/services/api/index${ext}`), tplApi)
   }
@@ -51,12 +50,12 @@ const init = async (projectName, options) => {
     const tplIndex = tpl('tpl_routes')
     writeFile(joinPath(projectDir, `/src/routes/index${ext}`), tplIndex)
   }
-  createPage('Home', {ts, redux})
+  createPage('Home', { ts, redux })
   console.log('\nHello World -v-')
 }
 
 function initDirections(options) {
-  const {redux, axios, router} = options
+  const { redux, axios, router } = options
   const curPath = process.cwd()
   const dirs = [joinPath(curPath, '/src/pages/home'), joinPath(__dirname, '/src/utils')]
   if (redux) {
@@ -73,25 +72,19 @@ function initDirections(options) {
 }
 
 function installDep(options) {
-  const {axios, router, redux, ts} = options
-
-  return new Promise((resolve, reject) => {
-    const {code} = shell.exec('yarn add dart-sass -D')
-    if (code === 0) {
-      resolve()
-    } else {
-      reject()
-    }
-  }).then(
-    res => {
-      if (axios || router || redux)
-        shell.exec(`yarn add ${axios ? 'axios' : ''} ${router ? 'react-router-dom' : ''} ${redux ? 'redux react-redux' : ''} ${ts ? '@types/react-redux' : ''}`)
-    },
-    err => {
-      console.log('意外退出')
-      console.log(code)
-    }
-  )
+  const { axios, router, redux, ts } = options
+  let code = 0
+  code = shell.exec('yarn add dart-sass -D')
+  if (axios && !code)
+    code = shell.exec(`yarn add axios`)
+  if (redux && !code)
+    code = shell.exec(`yarn add redux react-redux ${ts ? '@types/react-redux' : ''}`)
+  if (router && !code)
+    code = shell.exec(`yarn add react-router-dom`)
+  if (!code)
+    return
+  console.log('意外退出')
+  console.log(code)
 }
 
 module.exports = init
